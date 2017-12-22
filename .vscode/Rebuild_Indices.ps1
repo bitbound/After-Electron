44853cac30f59bc93609511ff9ba06cc3c8efc94
Jared Goodwin <translucency@outlook.com>
2017-12-21 20:10:23 -0800

    Updated PowerShell script with new index name.

--- .vscode/Rebuild_Indices.ps1
+++ .vscode/Rebuild_Indices.ps1
@@ -1,20 +1,20 @@
-Set-Content -Path ".\Components\Index.ts" -Value $null
+Set-Content -Path ".\Components\All.ts" -Value $null
 $NewContent = "";
 Get-ChildItem -Path ".\Components\" -Filter "*.ts" | ForEach-Object {
-    if ($_.Name -notlike "Index.ts") {
+    if ($_.Name -notlike "All.ts") {
         $FileName = $_.Name.Split(".")[0];
         $NewContent = "import * as $FileName from `"./$FileName`";`r`n" + $NewContent
         $NewContent = $NewContent +  "export { $FileName };`r`n"
     }
 }
-Set-Content -Path ".\Components\Index.ts" -Value $NewContent.Trim();
+Set-Content -Path ".\Components\All.ts" -Value $NewContent.Trim();
 
-Set-Content -Path ".\Models\Index.ts" -Value $null
+Set-Content -Path ".\Models\All.ts" -Value $null
 $NewContent = "";
 Get-ChildItem -Path ".\Models\" -Filter "*.ts" | ForEach-Object {
-    if ($_.Name -notlike "Index.ts") {
+    if ($_.Name -notlike "All.ts") {
         $FileName = $_.Name.Split(".")[0];
         $NewContent = $NewContent +  "export * from `"./$FileName`";`r`n" 
     }
 }
-Set-Content -Path ".\Models\Index.ts" -Value $NewContent.Trim();
+Set-Content -Path ".\Models\All.ts" -Value $NewContent.Trim();
