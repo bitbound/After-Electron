a21f7a08d97fdc580a76a55c4d065ce8e1b880fc
Jared Goodwin <translucency@outlook.com>
2018-01-13 10:17:43 -0800

    Fixed bug with Help related to commands being in their own module.

--- Commands/Help.ts
+++ Commands/Help.ts
@@ -1,11 +1,12 @@
 import { Command } from "../Models/All";
 import { Utilities, UI } from "../Components/All";
+import * as Commands from "../Commands/All";
 
 export default new Command("Help", "Global",
     `Provides help information for the supplied command (example: "Help Charge").<br><br>For a list of all commands, use "Help" by itself.`,
     function(){ 
-        return `${Utilities.GetHelpTitle(this)}List of commands (and category):<br>${Object.keys(exports).map((value)=>{
-            return `${(exports[value] as Command).Name}&nbsp;&nbsp;&nbsp;(${(exports[value] as Command).Category})`;
+        return `${Utilities.GetHelpTitle(this)}List of commands (and category):<br>${Object.keys(Commands).map((value)=>{
+            return `${(Commands[value] as Command).Name}&nbsp;&nbsp;&nbsp;(${(Commands[value] as Command).Category})`;
         }).join("<br>")}`;
     },
     function(parameters:Array<string>){
@@ -16,20 +17,20 @@ export default new Command("Help", "Global",
         if (parameters instanceof Array == false){
             parameters = [ parameters.toString() ];
         }
-        var commandParameter = Object.keys(exports).find(x=>x.toLowerCase() == parameters[0].toLowerCase());
+        var commandParameter = Object.keys(Commands).find(x=>x.toLowerCase() == parameters[0].toLowerCase());
         if (commandParameter) {
-            UI.AddSystemMessage((exports[commandParameter] as Command).GetFullHelp(), 1);
+            UI.AddSystemMessage((Commands[commandParameter] as Command).GetFullHelp(), 1);
         }
         else {
             var category = parameters[0].charAt(0).toUpperCase() + parameters[0].substring(1).toLowerCase();
             var categoryMatches = Array.from(new Set(
-                Object.keys(exports).filter(
-                    x=>(exports[x] as Command).Category.toLowerCase() == category.toLowerCase()).map((value, index)=>{
-                        return (exports[value] as Command).Category;
+                Object.keys(Commands).filter(
+                    x=>(Commands[x] as Command).Category.toLowerCase() == category.toLowerCase()).map((value, index)=>{
+                        return (Commands[value] as Command).Category;
                     })
                 ));
             if (categoryMatches.length == 1) {
-                var commandsInCategory = Object.keys(exports).filter(x=>(exports[x] as Command).Category == categoryMatches[0]).join("<br>");
+                var commandsInCategory = Object.keys(Commands).filter(x=>(Commands[x] as Command).Category == categoryMatches[0]).join("<br>");
                 var displayMessage = `<br><div style="display:inline-block; text-align:center; color: steelblue;">`;
                 for (var i = 0; i < categoryMatches[0].length + 15; i++){
                     displayMessage += "#";
